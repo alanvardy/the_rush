411f311dc8f3b9fbbf6985fa3ca1c954be7b67fe
Alan Vardy <alanvardy@gmail.com>
2020-02-29 08:13:14 -0800

    Add cell and sort helpers

--- lib/the_rush_web/views/page_view.ex
+++ lib/the_rush_web/views/page_view.ex
@@ -3,17 +3,22 @@ defmodule TheRushWeb.PageView do
 
   alias ExEffectiveBootstrap.Icons
 
-  @doc "Make a sort button and show a chevron if column is sorted"
-  @spec build_sort(any, any) :: {:safe, iolist} | String.t()
+  @doc "Make a sort button and show a chevron if column is sorted, takes the current column and the sort variable"
+  @spec build_sort(any, any) :: {:safe, iolist}
   def build_sort(col, {col, :desc}) do
-    content_tag(:span, ["sort", Icons.icon("chevron-down")], [])
+    content_tag(:span, ["sort", Icons.icon("chevron-down")], class: "text-muted")
   end
 
   def build_sort(col, {col, :asc}) do
-    content_tag(:span, ["sort", Icons.icon("chevron-up")], [])
+    content_tag(:span, ["sort", Icons.icon("chevron-up")], class: "text-muted")
   end
 
   def build_sort(_, _) do
-    "sort  "
+    content_tag(:span, "sort", class: "text-muted mr-2")
   end
+
+  @doc "Color the cell if it is in a sorted column, takes the current column and the sort variable"
+  @spec cell_class(String.t(), any) :: String.t()
+  def cell_class(column, {column, _}), do: "table-active"
+  def cell_class(_, _), do: ""
 end
