78adfbbc0e8d0d033c6188e849af292aaab3585d
Alan Vardy <alanvardy@gmail.com>
2020-02-29 09:51:37 -0800

    Add sort tests

--- test/the_rush/player_statistics/player_statistics_test.exs
+++ test/the_rush/player_statistics/player_statistics_test.exs
@@ -4,45 +4,87 @@ defmodule TheRush.PlayerStatistics.SortTest do
 
   alias TheRush.PlayerStatistics.Sort
 
-  @data [
-    %{"field1" => 456, "field2" => "hij", "field3" => :apple},
-    %{"field1" => 123, "field2" => "def", "field3" => :banana},
-    %{"field1" => 789, "field2" => "abc", "field3" => :carrot}
-  ]
+  @struct %{
+    data: [
+      %{"field1" => 123, "field2" => "def", "field3" => :banana},
+      %{"field1" => 456, "field2" => "hij", "field3" => :apple},
+      %{"field1" => 789, "field2" => "abc", "field3" => :carrot}
+    ],
+    sort: {"field1", :desc},
+    search: ""
+  }
 
   describe "run/1" do
     test "just returns the data when no sort is defined" do
-      assert Sort.run(%{data: @data}) == @data
-      assert Sort.run(%{data: @data, sort: nil}) == @data
-      assert Sort.run(%{data: @data, sort: :complete_nonsense}) == @data
+      assert Sort.run(@struct) == @struct
     end
 
     test "can sort descending" do
-      assert Sort.run(%{data: @data, sort: {"field1", :desc}}) == [
-               %{"field1" => 123, "field2" => "def", "field3" => :banana},
-               %{"field1" => 456, "field2" => "hij", "field3" => :apple},
-               %{"field1" => 789, "field2" => "abc", "field3" => :carrot}
-             ]
+      sort = {"field2", :desc}
+      input = %{@struct | sort: sort}
 
-      assert Sort.run(%{data: @data, sort: {"field2", :desc}}) == [
-               %{"field1" => 789, "field2" => "abc", "field3" => :carrot},
-               %{"field1" => 123, "field2" => "def", "field3" => :banana},
-               %{"field1" => 456, "field2" => "hij", "field3" => :apple}
-             ]
+      output = %{
+        sort: sort,
+        search: "",
+        data: [
+          %{"field1" => 789, "field2" => "abc", "field3" => :carrot},
+          %{"field1" => 123, "field2" => "def", "field3" => :banana},
+          %{"field1" => 456, "field2" => "hij", "field3" => :apple}
+        ]
+      }
+
+      assert Sort.run(input) == output
     end
-  end
 
-  test "can sort ascending" do
-    assert Sort.run(%{data: @data, sort: {"field1", :asc}}) == [
-             %{"field1" => 789, "field2" => "abc", "field3" => :carrot},
-             %{"field1" => 456, "field2" => "hij", "field3" => :apple},
-             %{"field1" => 123, "field2" => "def", "field3" => :banana}
-           ]
+    test "can sort descending again" do
+      sort = {"field3", :desc}
+      input = %{@struct | sort: sort}
 
-    assert Sort.run(%{data: @data, sort: {"field3", :asc}}) == [
-             %{"field1" => 789, "field2" => "abc", "field3" => :carrot},
-             %{"field1" => 123, "field2" => "def", "field3" => :banana},
-             %{"field1" => 456, "field2" => "hij", "field3" => :apple}
-           ]
+      output = %{
+        sort: sort,
+        search: "",
+        data: [
+          %{"field1" => 456, "field2" => "hij", "field3" => :apple},
+          %{"field1" => 123, "field2" => "def", "field3" => :banana},
+          %{"field1" => 789, "field2" => "abc", "field3" => :carrot}
+        ]
+      }
+
+      assert Sort.run(input) == output
+    end
+
+    test "can sort ascending" do
+      sort = {"field1", :asc}
+      input = %{@struct | sort: sort}
+
+      output = %{
+        sort: sort,
+        search: "",
+        data: [
+          %{"field1" => 789, "field2" => "abc", "field3" => :carrot},
+          %{"field1" => 456, "field2" => "hij", "field3" => :apple},
+          %{"field1" => 123, "field2" => "def", "field3" => :banana}
+        ]
+      }
+
+      assert Sort.run(input) == output
+    end
+
+    test "can sort ascending again" do
+      sort = {"field2", :asc}
+      input = %{@struct | sort: sort}
+
+      output = %{
+        sort: sort,
+        search: "",
+        data: [
+          %{"field1" => 456, "field2" => "hij", "field3" => :apple},
+          %{"field1" => 123, "field2" => "def", "field3" => :banana},
+          %{"field1" => 789, "field2" => "abc", "field3" => :carrot}
+        ]
+      }
+
+      assert Sort.run(input) == output
+    end
   end
 end
