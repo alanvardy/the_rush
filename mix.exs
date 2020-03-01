1220e8e585cc15e85a143b03f9c56e34652489fa
Alan Vardy <alanvardy@gmail.com>
2020-02-29 20:32:15 -0800

    Use elixir 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TheRush.MixProject do
     [
       app: :the_rush,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       test_coverage: [tool: ExCoveralls],
