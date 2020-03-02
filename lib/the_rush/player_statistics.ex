c3900752de43c382f837a789a6ed932f0d73aba0
Alan Vardy <alanvardy@gmail.com>
2020-03-02 15:22:45 -0800

    Namespace CSV functions

--- lib/the_rush/player_statistics.ex
+++ lib/the_rush/player_statistics.ex
@@ -1,8 +1,14 @@
 defmodule TheRush.PlayerStatistics do
   @moduledoc "Handles getting and sorting NFL player statistical data"
 
-  alias TheRush.PlayerStatistics.{Paginate, Query, Request, Search, Sort}
+  alias TheRush.PlayerStatistics.{Csv, Find, Paginate, Query, Request, Search, Sort}
 
+  @doc """
+  Runs a query through search, sort and paginate, rerunning each as needed.
+  If one runs then the others after need to alter the query as well
+
+  i.e. if Sort alters the query, then paginate needs to as well.
+  """
   @spec get(Query.t()) :: Query.t()
   def get(query) do
     query
@@ -15,4 +21,8 @@ defmodule TheRush.PlayerStatistics do
   defdelegate change_search(query, column), to: Search, as: :change
   defdelegate change_sort(query, column), to: Sort, as: :change
   defdelegate change_page(query, column), to: Paginate, as: :change
+
+  defdelegate find_serialized_pid(), to: Find, as: :serialized_pid
+  defdelegate find_query(serialized_pid), to: Find, as: :query
+  defdelegate export_csv(conn, query), to: Csv, as: :export
 end
