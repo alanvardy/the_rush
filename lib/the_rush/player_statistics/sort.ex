c2bd0ab0ae49cc173c930411ce36b1d7d85771d6
Alan Vardy <alanvardy@gmail.com>
2020-03-02 06:58:21 -0800

    add build_sort_fields

--- lib/the_rush/player_statistics/sort.ex
+++ lib/the_rush/player_statistics/sort.ex
@@ -5,20 +5,30 @@ defmodule TheRush.PlayerStatistics.Sort do
   @doc "For sorting data list on initial load"
   @spec presort([map]) :: [map]
   def presort(data) do
-    Enum.sort(data, &(&1["Player"] <= &2["Player"]))
+    Enum.sort(data, &(&1["Player"] >= &2["Player"]))
   end
+
+  # Alphanumeric columns
+  @alpha_columns ["Player", "Team", "Pos"]
+
+  # Columns that have digits mixed with other characters need to have them extracted
+  @mixed_columns ["Lng", "Yds"]
+
+  def mixed_columns, do: @mixed_columns
 
   @doc """
     Trigger a new sort next execute, which means the data will also
     need to be repaginated. Reset to first page.
+    Alphanumeric fields are sorted ascending to start
+    Numerical fields are sorted descending to start
   """
   @spec change(Query.t(), {String.t(), :asc | :desc}) :: Query.t()
   def change(%Query{sort: {col, direction}} = query, column) do
     sort =
-      if col == column do
-        {column, reverse(direction)}
-      else
-        {column, :desc}
+      case {column, col} do
+        {same, same} -> {col, reverse(direction)}
+        {column, _} when column in @alpha_columns -> {column, :asc}
+        {column, _} -> {column, :desc}
       end
 
     %Query{query | sort: sort, sorted_data: nil, paginated_data: nil, page: 1}
@@ -26,12 +36,18 @@ defmodule TheRush.PlayerStatistics.Sort do
 
   @doc "Run the sort if previously triggered"
   @spec execute(Query.t()) :: Query.t()
-  def execute(%Query{searched_data: searched_data, sort: {col, :desc}, sorted_data: nil} = query) do
-    %Query{query | sorted_data: Enum.sort(searched_data, &(&1[col] <= &2[col]))}
-  end
+  def execute(%Query{sorted_data: nil} = query) do
+    %Query{searched_data: searched_data, sort: {col, dir}} = query
 
-  def execute(%Query{searched_data: searched_data, sort: {col, :asc}, sorted_data: nil} = query) do
-    %Query{query | sorted_data: Enum.sort(searched_data, &(&1[col] >= &2[col]))}
+    sort_fn =
+      case {col, dir} do
+        {col, :asc} when col in @mixed_columns -> &(&1["#{col}_sort"] <= &2["#{col}_sort"])
+        {col, :desc} when col in @mixed_columns -> &(&1["#{col}_sort"] >= &2["#{col}_sort"])
+        {col, :asc} -> &(&1[col] <= &2[col])
+        {col, :desc} -> &(&1[col] >= &2[col])
+      end
+
+    %Query{query | sorted_data: Enum.sort(searched_data, sort_fn)}
   end
 
   # Pass through if no sorting needed
@@ -39,4 +55,40 @@ defmodule TheRush.PlayerStatistics.Sort do
 
   defp reverse(:asc), do: :desc
   defp reverse(:desc), do: :asc
+
+  @doc """
+  Puts a new processed sort field with the suffix _sort in the data for search
+  purposes, takes data and a list of the columns that need to be processed
+  """
+  @spec build_sort_fields([map], [String.t()]) :: [map]
+  def build_sort_fields(data, columns \\ @mixed_columns)
+  def build_sort_fields(data, []), do: data
+
+  def build_sort_fields(data, [column | tail]) do
+    data
+    |> Enum.map(&Map.put(&1, "#{column}_sort", extract_integer(&1, column)))
+    |> build_sort_fields(tail)
+  end
+
+  # Pull an integer out of the string, return 0 if no success
+  defp extract_integer(row, column) do
+    with lng <- row[column],
+         {:string, string} <- is_string?(lng),
+         {:ok, match} <- get_digits(string) do
+      String.to_integer(match)
+    else
+      {:number, number} -> number
+      {:error, :no_match} -> 0
+    end
+  end
+
+  defp is_string?(string) when is_binary(string), do: {:string, string}
+  defp is_string?(number), do: {:number, number}
+
+  defp get_digits(string) do
+    case Regex.scan(~r/\d/, string) do
+      [] -> {:error, :no_match}
+      list -> {:ok, Enum.join(list)}
+    end
+  end
 end
