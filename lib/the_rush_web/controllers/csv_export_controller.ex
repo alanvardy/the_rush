e3a68b65beb97ad03e6575a55132697bcbd3342f
Alan Vardy <alanvardy@gmail.com>
2020-03-02 15:23:58 -0800

    CSV namespacing

--- lib/the_rush_web/controllers/csv_export_controller.ex
+++ lib/the_rush_web/controllers/csv_export_controller.ex
@@ -1,11 +1,11 @@
 defmodule TheRushWeb.CsvExportController do
   use TheRushWeb, :controller
 
-  alias TheRush.PlayerStatistics.Csv
+  alias TheRush.PlayerStatistics
 
   @spec create(Plug.Conn.t(), any) :: Plug.Conn.t()
   def create(conn, %{"csv_export" => %{"serialized_pid" => serialized_pid}}) do
-    query = Csv.get_query(serialized_pid)
-    Csv.export(conn, query)
+    query = PlayerStatistics.find_query(serialized_pid)
+    PlayerStatistics.export_csv(conn, query)
   end
 end
